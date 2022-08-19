8ab5def814279d1d788558d060b38110874b47f1
ajen <2014301500137@whu.edu.cn>
2022-08-19 10:22:19 +0800

    调整测试代码适配运行在miri上;

--- channel/tests/simple.rs
+++ channel/tests/simple.rs
@@ -3,40 +3,40 @@ use std::{cell::RefCell, collections::BTreeSet, rc::Rc};
 
 #[tokio::test]
 async fn it_works() {
-	let now = std::time::Instant::now();
+	//let now = std::time::Instant::now();
 	tokio::task::LocalSet::new()
 		.run_until(async {
-			let (tx, rx) = bounded(1000);
+			let (tx, rx) = bounded(100);
 			let j = tokio::task::spawn_local(async move {
 				let mut it = 0..;
 				while let Ok(v) = rx.recv().await {
 					assert_eq!(v, it.next().unwrap());
 				}
 			});
-			for i in 0..1_000_000 {
+			for i in 0..100 {
 				tx.send(i).await.unwrap();
 			}
 			drop(tx);
 			j.await.unwrap();
 		})
 		.await;
-	println!("time cost: {}", now.elapsed().as_millis());
+	//println!("time cost: {}", now.elapsed().as_millis());
 
 	// flume
-	let now = std::time::Instant::now();
-	let (tx, rx) = flume::bounded(1000);
+	//let now = std::time::Instant::now();
+	let (tx, rx) = flume::bounded(100);
 	let j = tokio::spawn(async move {
 		let mut it = 0..;
 		while let Ok(v) = rx.recv_async().await {
 			assert_eq!(v, it.next().unwrap());
 		}
 	});
-	for i in 0..1_000_000 {
+	for i in 0..100 {
 		tx.send_async(i).await.unwrap();
 	}
 	drop(tx);
 	j.await.unwrap();
-	println!("flume time cost: {}", now.elapsed().as_millis());
+	//println!("flume time cost: {}", now.elapsed().as_millis());
 }
 
 #[test]
@@ -58,15 +58,15 @@ fn try_send_tests() {
 async fn test_drop_notify() {
 	tokio::task::LocalSet::new()
 		.run_until(async {
-			let (tx, rx) = bounded::<()>(1000);
-			let sleep_millis = 100;
-			let now = std::time::Instant::now();
+			let (tx, rx) = bounded::<()>(100);
+			//let sleep_millis = 100;
+			//let now = std::time::Instant::now();
 			let j = tokio::task::spawn_local(async move {
 				rx.recv().await.unwrap_err();
-				assert_eq!(sleep_millis/ 10, now.elapsed().as_millis() as u64/ 10);
+				//assert_eq!(sleep_millis/ 10, now.elapsed().as_millis() as u64/ 10);
 			});
 
-			tokio::time::sleep(std::time::Duration::from_millis(sleep_millis)).await;
+			//tokio::time::sleep(std::time::Duration::from_millis(sleep_millis)).await;
 			drop(tx);
 			j.await.unwrap();
 		})
@@ -82,8 +82,9 @@ fn test_leak() {
 		tx.try_send(Foo(i, trash_bin.clone())).unwrap();
 	}
 
+	let black_hole = Rc::new(RefCell::new(BTreeSet::new()));
 	for _ in 0..5 {
-		std::mem::forget(rx.try_recv().unwrap());
+		rx.try_recv().unwrap().1 = black_hole.clone();
 	}
 
 	drop(tx);
