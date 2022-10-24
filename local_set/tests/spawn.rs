03c9e21d016d1cc3d7d0438532ab66d28480cad5
ajen <2014301500137@whu.edu.cn>
2022-10-24 18:51:54 +0800

    Add futures::exector::LocalPool heavyspawn test for comparison;

--- local_set/tests/spawn.rs
+++ local_set/tests/spawn.rs
@@ -1,11 +1,12 @@
 #![cfg(test)]
 use std::{
 	cell::Cell,
-	future::{ready},
-	task::{self,   RawWaker, RawWakerVTable},
+	future::ready,
+	task::{self, RawWaker, RawWakerVTable},
 	time::Instant,
 };
 
+use futures::task::LocalSpawnExt;
 use local_set::{spawn_local, LocalSet};
 #[tokio::test]
 async fn spawn_test() {
@@ -30,14 +31,12 @@ fn noop_vtable() -> &'static RawWakerVTable {
 	&RawWakerVTable::new(|_| noop_waker_raw(), drop, drop, drop)
 }
 
-thread_local! { static C: Cell<i32> = Cell::new(0);}
 #[tokio::test]
 async fn heavy_spawn() {
 	let task_cnt = 100_000;
 
 	{
 		let mut local_set = LocalSet::default();
-		C.with(|c| c.set(0));
 		let now = Instant::now();
 		local_set.run_until(singleton_spawn_recure(task_cnt)).await;
 		println!("singleton: {}", now.elapsed().as_millis());
@@ -45,29 +44,45 @@ async fn heavy_spawn() {
 
 	{
 		let local_set = tokio::task::LocalSet::default();
-		C.with(|c| c.set(0));
 		let now = Instant::now();
 		local_set.run_until(tokio_spawn_recure(task_cnt)).await;
 		println!("tokio: {}", now.elapsed().as_millis());
 	}
+
+	{
+		let mut pool = futures::executor::LocalPool::new();
+		let now = Instant::now();
+		let spawner = pool.spawner();
+		SPAWNER.set(&spawner, || {
+			pool.run_until(futures_spawn_recure(task_cnt));
+		});
+		println!("futures: {}", now.elapsed().as_millis());
+	}
+}
+
+scoped_tls::scoped_thread_local!(static SPAWNER: futures::executor::LocalSpawner);
+async fn futures_spawn_recure(cnt: i32) {
+	if cnt > 0 {
+		SPAWNER
+			.with(|spawner| {
+				spawner
+					.spawn_local_with_handle(async move { futures_spawn_recure(cnt - 1).await })
+					.unwrap()
+			})
+			.await
+	}
 }
 
 async fn tokio_spawn_recure(cnt: i32) {
-	let cur_cnt = C.with(|c| c.get());
-	if cur_cnt < cnt {
-		C.with(|c| c.set(cur_cnt + 1));
-
-		let j = tokio::task::spawn_local(async move { tokio_spawn_recure(cnt).await });
+	if cnt > 0 {
+		let j = tokio::task::spawn_local(async move { tokio_spawn_recure(cnt - 1).await });
 		j.await.unwrap()
 	}
 }
 
 async fn singleton_spawn_recure(cnt: i32) {
-	let cur_cnt = C.with(|c| c.get());
-	if cur_cnt < cnt {
-		C.with(|c| c.set(cur_cnt + 1));
-
-		let j = local_set::spawn_local(async move { singleton_spawn_recure(cnt).await });
+	if cnt > 0 {
+		let j = local_set::spawn_local(async move { singleton_spawn_recure(cnt - 1).await });
 		j.await
 	}
 }
